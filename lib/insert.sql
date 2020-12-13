e65b337564fe366bc8dbb5892d0d3471ebc7b601
romeroraven <romeroravenr@gmail.com>
2020-12-12 17:16:52 -0700

    commit tree 2bf1b2be5cf5e7bc598ff130f828cb4f0f12d10b

---
+++ lib/insert.sql
@@ -0,0 +1,122 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+    VALUES ("Reuse", "Green", 4000, 11, 11);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+    VALUES ("Sea awareness", "Blue", 1000, 12, 12);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+    VALUES ("Earth project", "Brown", 5000, 13, 13);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+    VALUES ("Shelter for all", "Orange", 6000, 14, 14);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+    VALUES ("Together", "Purple", 7000, 15, 15);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+    VALUES ("For all", "Purple", 1000, 16, 16);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+    VALUES ("Tree us", "Green", 1000, 17, 17);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+    VALUES ("Everyone's chance", "Brown", 1500, 18, 18);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+    VALUES ("use", "Green", 4000, 11, 11);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+    VALUES ("awareness", "Blue", 1000, 12, 12);
+
+INSERT INTO users (name, age)
+    VALUES ("R", 1);
+INSERT INTO users (name, age)
+    VALUES ("K", 2);
+INSERT INTO users (name, age)
+    VALUES ("Q", 3);
+INSERT INTO users (name, age)
+    VALUES ("u", 4);
+INSERT INTO users (name, age)
+    VALUES ("i", 6);
+INSERT INTO users (name, age)
+    VALUES ("f", 7);
+INSERT INTO users (name, age)
+    VALUES ("df", 8);
+INSERT INTO users (name, age)
+    VALUES ("v", 9);
+INSERT INTO users (name, age)
+    VALUES ("T", 10);
+INSERT INTO users (name, age)
+    VALUES ("fd", 11);
+INSERT INTO users (name, age)
+    VALUES ("WE", 12);
+INSERT INTO users (name, age)
+    VALUES ("qq", 13);
+INSERT INTO users (name, age)
+    VALUES ("vvv", 14);
+INSERT INTO users (name, age)
+    VALUES ("f", 15);
+INSERT INTO users (name, age)
+    VALUES ("FF", 16);
+INSERT INTO users (name, age)
+    VALUES ("Qxxx", 17);
+INSERT INTO users (name, age)
+    VALUES ("Qxccxcxz", 18);
+INSERT INTO users (name, age)
+    VALUES ("Qxsxsxs", 19);
+INSERT INTO users (name, age)
+    VALUES ("cddc", 20) ;
+INSERT INTO users (name, age)
+    VALUES ("cytytc", 20) ;
+
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "qq", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "cddc", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (200, "WE", "use");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (4000, "vvv", "Reuse");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (100, "qq", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "qq", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "cddc", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (200, "WE", "use");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (4000, "vvv", "Reuse");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "qq", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "cddc", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (200, "WE", "use");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (4000, "vvv", "Reuse");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (100, "qq", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "qq", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "cddc", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (200, "WE", "use");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (4000, "vvv", "Reuse");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "qq", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "cddc", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (200, "WE", "use");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (4000, "vvv", "Reuse");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (100, "qq", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "qq", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "cddc", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (200, "WE", "use");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (4000, "vvv", "Reuse");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (2000, "cddc", "Together");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (200, "WE", "use");
+INSERT INTO pledges (amount, user_id, project_id)
+    VALUES (4000, "vvv", "Reuse");
